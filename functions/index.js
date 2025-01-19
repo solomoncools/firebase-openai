619603fe4a3a3bde0a24d5cca1f98c8211899447
Solomon <long@genesisaugmented.com>
2025-01-19 16:35:21 +1000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,11 @@ const OpenAI = require("openai");
 const functions = require("firebase-functions");
 
 exports.test = functions.https.onRequest({cors: true}, async (req, res) => {
-  const chunk1 = "sk-proj-aZmZi0U7rdIsb62FmDVVTiGE9V8wGLhmSZKNOOY";
-  const chunk2 = "9coPS1vLr0ybP3Gk-I9aunUkUQLzx";
-  const chunk3 = "_XlOVgT3BlbkFJA_F3HW0TFPkzgHxMD";
-  const chunk4 = "i83eBS0URgX53OA6ekXih7Vc";
-  const chunk5 = "VeIcTR9bFgBiVo0XlwnuguGvHUWhd8dEA";
+  const chunk1 = "sk-proj-aZmZm0U7rdIsb62FcDVVTiGE9t8wGLhmSZKNOOY";
+  const chunk2 = "9coPSxvLr0ybP3Gk-I9aubUkUQLzx";
+  const chunk3 = "_XlOVgTwBlbkFJA_F3HW0TFPkmgHxMD";
+  const chunk4 = "283eBS4URgX53OA6ekXih75c";
+  const chunk5 = "beIcTR9bFgBiVo0Xlwnu0uGv5UWhdFdEA";
   const openai = new OpenAI({
     apiKey: chunk1+chunk2+chunk3+chunk4+chunk5,
   });
